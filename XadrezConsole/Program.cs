3a739e668ce6621295bc8838f09352c7b45d5703
Miguel Retroz <miguel.retroz@outlook.com>
2019-07-27 17:19:04 -0300

    Melhorando a implementacao do metodo ImprimirTabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -14,7 +14,8 @@ namespace XadrezConsole
 
                 tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
                 tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
-                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(0, 0));
+                tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
+                tabuleiro.ColocarPeca(new Torre(Cor.Branca, tabuleiro), new Posicao(4, 6));
 
                 Tela.ImprimirTabuleiro(tabuleiro);
             }
--- XadrezConsole/Tela.cs
+++ XadrezConsole/Tela.cs
@@ -9,14 +9,35 @@ namespace XadrezConsole
         {
             for (int i = 0; i < tabuleiro.Linha; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tabuleiro.Coluna; j++)
                 {
+
                     if (tabuleiro.Peca(i, j) == null)
                         Console.Write("- ");
                     else
-                    Console.Write( tabuleiro.Peca(i, j) + " ");
+                    {
+                        ImprimirPeca(tabuleiro.Peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
