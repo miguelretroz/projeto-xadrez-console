ab2a03e4774d03b66ec754b8e4c8baf09afe1bd8
Miguel Retroz <miguel.retroz@outlook.com>
2019-07-31 19:07:24 -0300

    Jogada especial promocao

--- XadrezConsole/Xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/Xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,21 @@ namespace Xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = Tabuleiro.Peca(destino);
+
+            // #Jogada especia promocao
+            if (p is Peao)
+            {
+                if ((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tabuleiro.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(p.Cor, Tabuleiro);
+                    Tabuleiro.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -158,7 +173,7 @@ namespace Xadrez
                 MudaJogador();
             }
             
-            Peca p = Tabuleiro.Peca(destino);
+
             // # Jogada especial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
             {
